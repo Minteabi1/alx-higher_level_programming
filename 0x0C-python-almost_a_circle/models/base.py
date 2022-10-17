5c8d5ff17f154446ea2042d31ac37afc5c3a9675
Minteabi1 <mintesnottsegaye40@gmail.com>
2022-10-17 10:08:09 +0300

    pep8

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -58,15 +58,13 @@ class Base:
     @classmethod
     def load_from_file(cls):
         filename = cls.__name__ + ".json"
-        l = []
+        ly = []
         try:
             with open(filename, 'r') as f:
-                l = cls.from_json_string(f.read())
+                ly = cls.from_json_string(f.read())
             for i, e in enumerate(l):
-                l[i] = cls.create(**l[i])
-        except:
-            pass
-        return l
+                ly[i] = cls.create(**l[i])
+        return ly
 
     @classmethod
     def save_to_file_csv(cls, list_objs):
@@ -86,7 +84,7 @@ class Base:
     def load_from_file_csv(cls):
         """deserializes a list of Rectangles/Squares in csv"""
         filename = cls.__name__ + ".csv"
-        l = []
+        ly = []
         try:
             with open(filename, 'r') as csvfile:
                 csv_reader = csv.reader(csvfile)
@@ -101,10 +99,8 @@ class Base:
                         dictionary = {"id": int(args[0]), "size": int(args[1]),
                                       "x": int(args[2]), "y": int(args[3])}
                     obj = cls.create(**dictionary)
-                    l.append(obj)
-        except:
-            pass
-        return l
+                    ly.append(obj)
+        return ly
 
     @staticmethod
     def draw(list_rectangles, list_squares):
